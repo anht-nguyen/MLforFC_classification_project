1c3d3f53f130e5830a94c8d1df31d294de768a99
Anh T. Nguyen <tuananh.nguyen28898@gmail.com>
2025-05-19 14:42:41 -0400

    Setup evaluation process for Spectral Granger only to speedup

--- classification_AO_conditions/redo_evaluation/redo_evaluation_on_best_models_DL.py
+++ classification_AO_conditions/redo_evaluation/redo_evaluation_on_best_models_DL.py
@@ -80,137 +80,138 @@ def main():
     all_evals = {}
 
     for FC_name, content in meta.items():
-        print(f"\n=== {selected_model} on FC = {FC_name} ===")
-        best_params = content[selected_model]['best_params']
-        print('Best params:', best_params)
+        if FC_name == 'Spectral Granger':
+            print(f"\n=== {selected_model} on FC = {FC_name} ===")
+            best_params = content[selected_model]['best_params']
+            print('Best params:', best_params)
 
-        # Build dataset
-        full_ds = build_full_dataset(FC_name, selected_model)
+            # Build dataset
+            full_ds = build_full_dataset(FC_name, selected_model)
 
-        # Cross-validator
-        rkf = RepeatedKFold(n_splits=K_FOLDS,
-                            n_repeats=NUM_REPEATS_FINAL,
-                            random_state=42)
+            # Cross-validator
+            rkf = RepeatedKFold(n_splits=K_FOLDS,
+                                n_repeats=NUM_REPEATS_FINAL,
+                                random_state=42)
 
-        y_true_all, y_pred_all, y_score_all = [], [], []
-        train_splits, test_splits = [], []
+            y_true_all, y_pred_all, y_score_all = [], [], []
+            train_splits, test_splits = [], []
 
-        # Loop folds
-        for fold, (train_idx, test_idx) in enumerate(rkf.split(full_ds), start=1):
-            print(f"Fold {fold}/{K_FOLDS*NUM_REPEATS_FINAL}")
-            train_splits.append(train_idx)
-            test_splits.append(test_idx)
+            # Loop folds
+            for fold, (train_idx, test_idx) in enumerate(rkf.split(full_ds), start=1):
+                print(f"Fold {fold}/{K_FOLDS*NUM_REPEATS_FINAL}")
+                train_splits.append(train_idx)
+                test_splits.append(test_idx)
 
-            # Create loaders
-            bs = best_params.get('Batch Size', 64)
-            if selected_model == 'GCN':
-                from torch_geometric.loader import DataLoader as GeoLoader
-                ds_tr = Subset(full_ds.graphs, train_idx)
-                ds_te = Subset(full_ds.graphs, test_idx)
-                loader_tr = GeoLoader(ds_tr, batch_size=bs, shuffle=True)
-                loader_te = GeoLoader(ds_te, batch_size=bs, shuffle=False)
-            else:
-                ds_tr = Subset(full_ds, train_idx)
-                ds_te = Subset(full_ds, test_idx)
-                loader_tr = DataLoader(ds_tr, batch_size=bs, shuffle=True)
-                loader_te = DataLoader(ds_te, batch_size=bs, shuffle=False)
+                # Create loaders
+                bs = best_params.get('Batch Size', 64)
+                if selected_model == 'GCN':
+                    from torch_geometric.loader import DataLoader as GeoLoader
+                    ds_tr = Subset(full_ds.graphs, train_idx)
+                    ds_te = Subset(full_ds.graphs, test_idx)
+                    loader_tr = GeoLoader(ds_tr, batch_size=bs, shuffle=True)
+                    loader_te = GeoLoader(ds_te, batch_size=bs, shuffle=False)
+                else:
+                    ds_tr = Subset(full_ds, train_idx)
+                    ds_te = Subset(full_ds, test_idx)
+                    loader_tr = DataLoader(ds_tr, batch_size=bs, shuffle=True)
+                    loader_te = DataLoader(ds_te, batch_size=bs, shuffle=False)
 
-            # Instantiate model
-            if selected_model == 'CNN':
-                model = CNNClassifier(
-                    in_channels=1,
-                    hidden_dim=best_params['CNN-hidden'],
-                    num_classes=NUM_CLASSES,
-                    kernel_size0=best_params['Kernel Size 0'],
-                    kernel_size1=best_params['Kernel Size 1'],
-                    padding=best_params['Padding']
-                )
-            elif selected_model == 'MLP':
-                model = MLPClassifier(
-                    input_size=NUM_CHANNELS,
-                    hidden_dim=best_params['MLP-hidden'],
-                    num_layers=best_params['N-MLP'],
-                    num_classes=NUM_CLASSES
-                )
-            else:
-                model = GCN(
-                    in_channels=NUM_FREQS,
-                    hidden_dim=best_params['GCN-hidden'],
-                    num_layers=best_params['N-GCN'],
-                    dropout=best_params['Dropout'],
-                    drop_edge=best_params['DropEdge'],
-                    num_classes=NUM_CLASSES
-                )
-            model.to(DEVICE)
+                # Instantiate model
+                if selected_model == 'CNN':
+                    model = CNNClassifier(
+                        in_channels=1,
+                        hidden_dim=best_params['CNN-hidden'],
+                        num_classes=NUM_CLASSES,
+                        kernel_size0=best_params['Kernel Size 0'],
+                        kernel_size1=best_params['Kernel Size 1'],
+                        padding=best_params['Padding']
+                    )
+                elif selected_model == 'MLP':
+                    model = MLPClassifier(
+                        input_size=NUM_CHANNELS,
+                        hidden_dim=best_params['MLP-hidden'],
+                        num_layers=best_params['N-MLP'],
+                        num_classes=NUM_CLASSES
+                    )
+                else:
+                    model = GCN(
+                        in_channels=NUM_FREQS,
+                        hidden_dim=best_params['GCN-hidden'],
+                        num_layers=best_params['N-GCN'],
+                        dropout=best_params['Dropout'],
+                        drop_edge=best_params['DropEdge'],
+                        num_classes=NUM_CLASSES
+                    )
+                model.to(DEVICE)
 
-            # Optimizer & loss
-            optimizer = Adam(model.parameters(), lr=best_params['Learning Rate'], weight_decay=1e-4)
-            criterion = torch.nn.CrossEntropyLoss()
+                # Optimizer & loss
+                optimizer = Adam(model.parameters(), lr=best_params['Learning Rate'], weight_decay=1e-4)
+                criterion = torch.nn.CrossEntropyLoss()
 
-            # Train
-            if selected_model == 'GCN':
-                train_gnn(model, loader_tr, optimizer, criterion, NUM_EPOCH_FINAL, DEVICE, PATIENCE)
-                y_t, y_p, y_s = evaluate_gnn(model, loader_te, DEVICE)
-            else:
-                train_model(model, selected_model, NUM_EPOCH_FINAL, criterion,
-                            optimizer, loader_tr, DEVICE, PATIENCE)
-                y_t, y_p, y_s = evaluate_model(model, loader_te, DEVICE)
+                # Train
+                if selected_model == 'GCN':
+                    train_gnn(model, loader_tr, optimizer, criterion, NUM_EPOCH_FINAL, DEVICE, PATIENCE)
+                    y_t, y_p, y_s = evaluate_gnn(model, loader_te, DEVICE)
+                else:
+                    train_model(model, selected_model, NUM_EPOCH_FINAL, criterion,
+                                optimizer, loader_tr, DEVICE, PATIENCE)
+                    y_t, y_p, y_s = evaluate_model(model, loader_te, DEVICE)
 
-            y_true_all.extend(y_t)
-            y_pred_all.extend(y_p)
-            y_score_all.extend(y_s)
+                y_true_all.extend(y_t)
+                y_pred_all.extend(y_p)
+                y_score_all.extend(y_s)
 
-        # Aggregate
-        y_true = np.array(y_true_all)
-        y_pred = np.array(y_pred_all)
-        y_score = np.vstack(y_score_all)
+            # Aggregate
+            y_true = np.array(y_true_all)
+            y_pred = np.array(y_pred_all)
+            y_score = np.vstack(y_score_all)
 
-        # Save predictions
-        ckpt_file = os.path.join(ckpt_dir, f"{selected_model}_{FC_name}_checkpoint.npz")
-        np.savez_compressed(ckpt_file,
-                             y_true=y_true,
-                             y_pred=y_pred,
-                             y_score=y_score,
-                             train_splits=np.array(train_splits, dtype=object),
-                             test_splits=np.array(test_splits,  dtype=object)
-)
+            # Save predictions
+            ckpt_file = os.path.join(ckpt_dir, f"{selected_model}_{FC_name}_checkpoint.npz")
+            np.savez_compressed(ckpt_file,
+                                y_true=y_true,
+                                y_pred=y_pred,
+                                y_score=y_score,
+                                train_splits=np.array(train_splits, dtype=object),
+                                test_splits=np.array(test_splits,  dtype=object)
+    )
 
-        # Compute metrics
-        metrics = {}
-        metrics['accuracy'] = accuracy_score(y_true, y_pred)
-        metrics['balanced_accuracy'] = balanced_accuracy_score(y_true, y_pred)
-        # Precision/Recall/F1
-        prec_mac, rec_mac, f1_mac, _ = precision_recall_fscore_support(y_true, y_pred, average='macro')
-        prec_mic, rec_mic, f1_mic, _ = precision_recall_fscore_support(y_true, y_pred, average='micro')
-        metrics.update({
-            'precision_macro': prec_mac,
-            'recall_macro': rec_mac,
-            'f1_macro': f1_mac,
-            'precision_micro': prec_mic,
-            'recall_micro': rec_mic,
-            'f1_micro': f1_mic
-        })
-        # Jaccard index
-        metrics['jaccard_macro'] = jaccard_score(y_true, y_pred, average='macro')
-        metrics['jaccard_micro'] = jaccard_score(y_true, y_pred, average='micro')
-        # ROC AUC
-        metrics['auc_macro'] = roc_auc_score(y_true, y_score, multi_class='ovr', average='macro')
-        metrics['auc_micro'] = roc_auc_score(y_true, y_score, multi_class='ovr', average='micro')
-        # Confusion matrix and classification report
-        metrics['confusion_matrix'] = confusion_matrix(y_true, y_pred).tolist()
-        metrics['classification_report'] = classification_report(y_true, y_pred, output_dict=True)
+            # Compute metrics
+            metrics = {}
+            metrics['accuracy'] = accuracy_score(y_true, y_pred)
+            metrics['balanced_accuracy'] = balanced_accuracy_score(y_true, y_pred)
+            # Precision/Recall/F1
+            prec_mac, rec_mac, f1_mac, _ = precision_recall_fscore_support(y_true, y_pred, average='macro')
+            prec_mic, rec_mic, f1_mic, _ = precision_recall_fscore_support(y_true, y_pred, average='micro')
+            metrics.update({
+                'precision_macro': prec_mac,
+                'recall_macro': rec_mac,
+                'f1_macro': f1_mac,
+                'precision_micro': prec_mic,
+                'recall_micro': rec_mic,
+                'f1_micro': f1_mic
+            })
+            # Jaccard index
+            metrics['jaccard_macro'] = jaccard_score(y_true, y_pred, average='macro')
+            metrics['jaccard_micro'] = jaccard_score(y_true, y_pred, average='micro')
+            # ROC AUC
+            metrics['auc_macro'] = roc_auc_score(y_true, y_score, multi_class='ovr', average='macro')
+            metrics['auc_micro'] = roc_auc_score(y_true, y_score, multi_class='ovr', average='micro')
+            # Confusion matrix and classification report
+            metrics['confusion_matrix'] = confusion_matrix(y_true, y_pred).tolist()
+            metrics['classification_report'] = classification_report(y_true, y_pred, output_dict=True)
 
-        all_evals[FC_name] = {
-            'best_params': best_params,
-            'metrics': metrics,
-            'checkpoint': ckpt_file
-        }
+            all_evals[FC_name] = {
+                'best_params': best_params,
+                'metrics': metrics,
+                'checkpoint': ckpt_file
+            }
 
-        # Display summary
-        print(json.dumps(metrics, indent=2))
+            # Display summary
+            print(json.dumps(metrics, indent=2))
 
     # Save summary
-    out_json = os.path.join(this_dir, f"re_evaluation_{selected_model}_full_metrics.json")
+    out_json = os.path.join(this_dir, f"re_evaluation_{selected_model}_full_metrics_SpcG.json")
     with open(out_json, 'w') as f:
         json.dump({selected_model: all_evals}, f, indent=2)
     print(f"\n✅ Saved full metrics to {out_json}")
